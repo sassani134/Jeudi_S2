d295d0aea62fb2a063aa05253c2863d01869117e
Samuel <sassani134@gmail.com>
2018-10-11 22:10:14 +0200

    exo pas terminé

--- Trader.rb
+++ Trader.rb
@@ -12,11 +12,26 @@ end
 
 def name_of_cryptocurrency
 doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-	#doc.css('tbody').each do |link|
-	doc.xpath('/html/body/').each do |link|
+	doc.css('tbody').css('tr').css('a.currency-name-container.link-secondary').each do |link|
 	puts link.content
+	#parse_page.css(‘.content’).css(‘.row’).css(‘.hdrlnk’)
 end
+end
+def price_in_currency
+	doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+	doc.css('tbody').css('tr').css('td.no-wrap.market-cap.text-right').each do |link|
+	puts link.text
+end
+end
+def arrays_in_hash(methode1, methode2)
+	liste_crypto = Hash.new
+	methode1.count.times do |i|
+		liste_crypto[methode1[i]] = methode2[i]
+	end
+
+	return liste_crypto
 end
 
 #every_hour(name_of_cryptocurrency)
-name_of_cryptocurrency
+#name_of_cryptocurrency
+price_in_currency
